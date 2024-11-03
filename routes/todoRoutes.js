e2cf9fb89b008a253f26983c6f90344fbbdc8e0d
HarikeshKushwaha18 <harikeshk566@gmail.com>
2024-11-03 15:37:13 +0530

    Authentication done

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -5,16 +5,16 @@ const {signup} = require('../controllers/signUp');
 const {login} = require('../controllers/login');
 const {createTask, getTasks, updateStatus, deleteTask, gettasksByStatus} = require('../controllers/taskController');
 
-// const {auth} = require('../middleware/auth')
+const {auth} = require('../middleware/auth')
 
 router.post('/signup', signup);
 router.post('/login', login);
 
-router.post('/createTask',createTask);
-router.post('/update', updateStatus);
+router.post('/createTask', auth, createTask);
+router.post('/update',auth,  updateStatus);
 router.post('/deleteTask', deleteTask)
 
-router.get('/getTasks', getTasks);
-router.get('/getTasksByStatus/:userId', gettasksByStatus)
+router.get('/getTasks', auth, getTasks);
+router.get('/getTasksByStatus/:userId',auth, gettasksByStatus)
 
 module.exports = router;
